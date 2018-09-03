5406e936ac44c8a286bd48ccf7ba479616943cbf
bartolomeoadrian <35029613+bartolomeoadrian@users.noreply.github.com>
2018-09-03 02:06:39 -0300

    Pequeños cambios sesión

--- App/app.js
+++ App/app.js
@@ -20,51 +20,65 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use("/public", express.static(path.join(__dirname, 'public')));
 app.set('view engine','ejs');
 
+
 //ROUTING
 app.get('/', function(req,res){
-    const sessionCookie = req.cookies.session || '';
+    const sessionCookie = req.cookies.session || "";
     admin.auth().verifySessionCookie(sessionCookie, true).then((decodedClaims) => {
         res.render('index', { user: decodedClaims });
     }).catch(error => {
         res.render('index', { user: null });
     });
 });
+
 app.get('/login', function(req,res){
-    res.render('login');
+    const sessionCookie = req.cookies.session || "";
+    admin.auth().verifySessionCookie(sessionCookie, true).then((decodedClaims) => {
+        res.header(302, 
+            {'Location': 'profile'}
+        );
+    }).catch(error => {
+        res.render('login', { user: null });
+    });
+});
+
+app.get('/register', function(req,res){
+    const sessionCookie = req.cookies.session || "";
+    admin.auth().verifySessionCookie(sessionCookie, true).then((decodedClaims) => {
+        res.header(302, 
+            {'Location': 'profile'}
+        );
+    }).catch(error => {
+        res.render('register', { user: null });
+    });
 });
 
 app.get('/logout', function(req,res){
-    admin.auth().signOut();
-        
-    res.writeHead(301,
-        {Location: '/'}
-      );
+    res.clearCookie('session');
+    res.writeHead(302, 
+        {'Location': '/'}
+    );
     res.end();
 });
 
 app.post('/api/login', function(req,res){
     var idToken = req.body.token;
 
-    if(idToken.length == 0) {
-        res.clearCookie('session');
-        res.end();
-    } else {
-        admin.auth().verifyIdToken(idToken).then(function(decodedToken) {
-            var uid = decodedToken.uid;
-            console.log("Se logueo bien: " + uid);
-            
-            const expiresIn = 60 * 60 * 24 * 5 * 1000;
-            admin.auth().createSessionCookie(idToken, {expiresIn}).then((sessionCookie) => {
-                const options = {maxAge: expiresIn, httpOnly: true, secure: false};
-                res.cookie('session', sessionCookie, options);
-                res.end(JSON.stringify({status: 'success'}));
-            }, error => {;
-                res.end();
-            });
-        }).catch(function(error) {
+    admin.auth().verifyIdToken(idToken).then(function(decodedToken) {
+        var uid = decodedToken.uid;
+        
+        const expiresIn = 60 * 60 * 24 * 5 * 1000;
+        admin.auth().createSessionCookie(idToken, {expiresIn}).then((sessionCookie) => {
+            const options = {maxAge: expiresIn, httpOnly: true, secure: false};
+            res.cookie('session', sessionCookie, options);
+            console.log("Cookie creada = " + uid);
+            res.end();
+        }, error => {;
             res.end();
         });
-    }
+    }).catch(function(error) {
+        res.end();
+    });
 });
 
 app.listen(8080, function(){
